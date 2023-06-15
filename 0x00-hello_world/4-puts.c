c63c2adbddaf1742a779387b3f69795ba4640961
Masanabo98 <you@example.com>
2023-06-15 03:58:11 -0700

    a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stduo.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
-	retur (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
