d855b87f7079c219aebe97e1184bbb95bdc654d6
Masanabo98 <you@example.com>
2023-06-20 07:51:18 -0700

    prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - main block
- *
- * Return: 0.
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
-
 {
-	int c = 0;
-	int sum = 0;
-
-	while (c < 1024)
-	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-		c++;
-	}
-	printf("%i\n", sum);
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
